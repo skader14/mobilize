0ad876ff2454b329c96e8e19b42b3b93a226f7ca
skader14 <saifkader14@gmail.com>
2025-02-01 16:31:11 -0600

    first push! yippee!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,12 @@
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, TextInput, Dimensions } from "react-native";
+import { useState } from "react";
+import MapView from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedView } from "@/components/ThemedView";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function HomeScreen() {
   // UT Austin coordinates
@@ -12,12 +17,31 @@ export default function HomeScreen() {
     longitudeDelta: 0.01,
   };
 
+  const [searchText, setSearchText] = useState("");
+
   return (
     <ThemedView style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={utAustinRegion}
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        onPress={(data, details = null) => {
+          console.log(data, details);
+        }}
+        query={{
+          key: "AIzaSyC0FdW0B5CE6hdM1sC6BCwUwpl5tFL949s",
+          language: "en",
+        }}
+        styles={{
+          textInputContainer: styles.searchBar,
+        }}
       />
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Where to?"
+        value={searchText}
+        placeholderTextColor="gray"
+        onChangeText={setSearchText}
+      />
+      <MapView style={styles.map} initialRegion={utAustinRegion} />
     </ThemedView>
   );
 }
@@ -27,7 +51,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
+  },
+  searchBar: {
+    position: "absolute",
+    top: "7.5%",
+    left: 20,
+    right: 20,
+    zIndex: 1,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 5,
+    height: 45,
+    color: "black",
   },
 });
